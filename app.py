b4ff2decad6dfe7ab8876674a084dcabff02083c
Bhavik P <bhavikjainhp@gmail.com>
2021-10-10 16:40:51 +0530

    final commit

--- app.py
+++ app.py
@@ -21,8 +21,4 @@ def viewData():
     return render_template('Table/Table.html')
 
 if __name__ == "__main__":
-    app.run(
-        host = "localhost",
-        port = 5000,
-        debug=True
-    )
+    app.run()
--- data.py
+++ data.py
@@ -12,13 +12,13 @@ def search(name, top=8):
     Y = name
     Y_list = word_tokenize(Y)
     for z in range(len(result['Title'])):
-        
+
         X = str(result['Title'][z])
-        
+
 
         # tokenization
         X_list = word_tokenize(X)
-        
+
 
         # sw contains the list of stopwords
         sw = ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', "you're", "you've", "you'll", "you'd", 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', "she's", 'her', 'hers', 'herself', 'it', "it's", 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', "that'll", 'these', 'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', "don't", 'should', "should've", 'now', 'd', 'll', 'm', 'o', 're', 've', 'y', 'ain', 'aren', "aren't", 'couldn', "couldn't", 'didn', "didn't", 'doesn', "doesn't", 'hadn', "hadn't", 'hasn', "hasn't", 'haven', "haven't", 'isn', "isn't", 'ma', 'mightn', "mightn't", 'mustn', "mustn't", 'needn', "needn't", 'shan', "shan't", 'shouldn', "shouldn't", 'wasn', "wasn't", 'weren', "weren't", 'won', "won't", 'wouldn', "wouldn't"]
@@ -42,7 +42,7 @@ def search(name, top=8):
                 c+= l1[i]*l2[i]
         cosine = c / float((sum(l1)*sum(l2))**0.5)
         lst.append(cosine)
-    
+
     lst1= sorted(((value, index) for index, value in enumerate(lst)), reverse=True)
     for m in range(top):
         #course_title,  course URL, company, stars, enrol, description
